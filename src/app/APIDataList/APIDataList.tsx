6e3b2ec013a719b236bdd55b97a4ae22fc2417a3
Christie Molloy <cmolloy@redhat.com>
2020-10-07 14:19:31 -0400

    fix dynamic action set states

--- src/app/APIDataList/APIDataList.tsx
+++ src/app/APIDataList/APIDataList.tsx
@@ -30,15 +30,34 @@ import mockDataBackend from './../../mockdata-backend.json';
 
 const APIDataList: React.FunctionComponent = () => {
 
-  const [ isOpen, setIsOpen ] = useState(false);
+  const [ isOpenArray, setIsOpenArray ] = useState([]);
 
-  const onSelect = () => {
-    setIsOpen(isOpen);
-  };
+  // const onSelect = () => {
+  //   setIsOpen(isOpen);
+  // };
 
-  const onToggle = () => {
-    setIsOpen(isOpen);
-  };
+  function handleChange(e) {
+
+    const item = e.target.id;
+    var indexOfItem = isOpenArray.indexOf(item);
+
+    if(indexOfItem !== -1) {
+      var array = [...isOpenArray];
+      if(indexOfItem === 0) {
+        array.shift();
+        setIsOpenArray(array);
+      }
+      else {
+        var newArray = array.splice(indexOfItem, 1);
+        setIsOpenArray(newArray);
+      }
+    }
+    else {
+      setIsOpenArray( [item, ...isOpenArray]);
+    }
+  }
+
+  console.log('what is the open array' + isOpenArray + typeof(isOpenArray));
 
   const APIDataListItem = mockDataAPI.map((row, index) => {
     return (
@@ -64,25 +83,25 @@ const APIDataList: React.FunctionComponent = () => {
         >
           <Dropdown
             isPlain
-            id="actions-menu"
             position={DropdownPosition.right}
-            isOpen={isOpen}
-            onSelect={onSelect}
-            toggle={<KebabToggle onToggle={onToggle} />}
+            isOpen={isOpenArray.indexOf(`toggle-${index}`) !== -1 ? true : false}
+            // onSelect={onSelect}
+            onClick={handleChange}
+            toggle={<KebabToggle id={`toggle-${index}`} onToggle={handleChange}  />}
             dropdownItems={[
-              <DropdownItem key="link" href="/">
+              <DropdownItem key={`link-${index}`} href="/">
                 Overview
               </DropdownItem>,
-              <DropdownItem key="link" href="/">
+              <DropdownItem key={`link-${index}`} href="/">
                 Analytics
               </DropdownItem>,
-              <DropdownItem key="link" href="/">
+              <DropdownItem key={`link-${index}`} href="/">
                 Applications
               </DropdownItem>,
-              <DropdownItem key="link" href="/">
+              <DropdownItem key={`link-${index}`} href="/">
                 ActiveDocs
               </DropdownItem>,
-              <DropdownItem key="link" href="/">
+              <DropdownItem key={`link-${index}`} href="/">
                 Integration
               </DropdownItem>
             ]}
@@ -93,54 +112,54 @@ const APIDataList: React.FunctionComponent = () => {
     )
   });
 
-  const BackendDataListItem = mockDataBackend.map((row) => {
-    return (
-      <DataListItem aria-labelledby="single-action-item1">
-      <DataListItemRow>
-        <DataListItemCells
-          dataListCells={[
-            <DataListCell key="primary content">
-              <a href="" id="single-action-item1">
-                {row.name}
-              </a>
-            </DataListCell>,
-            <DataListCell key="secondary content" className="dashboard-list-secondary">
-              {row.timeCreated}
-            </DataListCell>
-          ]}
-        />
-        <DataListAction
-          aria-labelledby="multi-actions-item1 multi-actions-action1"
-          id="multi-actions-action1"
-          aria-label="Actions"
-          isPlainButtonAction
-        >
-          <Dropdown
-            isPlain
-            position={DropdownPosition.right}
-            isOpen={isOpen}
-            onSelect={onSelect}
-            toggle={<KebabToggle onToggle={onToggle} />}
-            dropdownItems={[
-              <DropdownItem key="link">
-                Overview
-              </DropdownItem>,
-              <DropdownItem key="link">
-                Analytics
-              </DropdownItem>,
-              <DropdownItem key="link">
-                Methods and Metrics
-              </DropdownItem>,
-              <DropdownItem key="link">
-                Mapping Rules
-              </DropdownItem>
-            ]}
-          />
-        </DataListAction>
-      </DataListItemRow>
-    </DataListItem>
-    )
-  });
+  // const BackendDataListItem = mockDataBackend.map((row) => {
+  //   return (
+  //     <DataListItem aria-labelledby="single-action-item1">
+  //     <DataListItemRow>
+  //       <DataListItemCells
+  //         dataListCells={[
+  //           <DataListCell key="primary content">
+  //             <a href="" id="single-action-item1">
+  //               {row.name}
+  //             </a>
+  //           </DataListCell>,
+  //           <DataListCell key="secondary content" className="dashboard-list-secondary">
+  //             {row.timeCreated}
+  //           </DataListCell>
+  //         ]}
+  //       />
+  //       <DataListAction
+  //         aria-labelledby="multi-actions-item1 multi-actions-action1"
+  //         id="multi-actions-action1"
+  //         aria-label="Actions"
+  //         isPlainButtonAction
+  //       >
+  //         <Dropdown
+  //           isPlain
+  //           position={DropdownPosition.right}
+  //           isOpen={isOpen}
+  //           onSelect={onSelect}
+  //           toggle={<KebabToggle onToggle={onToggle} />}
+  //           dropdownItems={[
+  //             <DropdownItem key="link">
+  //               Overview
+  //             </DropdownItem>,
+  //             <DropdownItem key="link">
+  //               Analytics
+  //             </DropdownItem>,
+  //             <DropdownItem key="link">
+  //               Methods and Metrics
+  //             </DropdownItem>,
+  //             <DropdownItem key="link">
+  //               Mapping Rules
+  //             </DropdownItem>
+  //           ]}
+  //         />
+  //       </DataListAction>
+  //     </DataListItemRow>
+  //   </DataListItem>
+  //   )
+  // });
 
   return (
     <PageSection>
@@ -199,7 +218,7 @@ const APIDataList: React.FunctionComponent = () => {
             </CardHeader>
             <CardBody>
               <DataList aria-label="single action data list example">
-                {BackendDataListItem}
+                {/* {BackendDataListItem} */}
               </DataList>
             </CardBody>
             <CardFooter>
